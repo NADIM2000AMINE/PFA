96dfa7c816b47e5fe5e591559cfed0dad38d91bf
mou3aad <mouadraf4@gmail.com>
2023-06-01 16:05:53 +0100

    added a new way to return to signIn for from the ForgotPassword form.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import * as Components from './Components/Auth_Components';
 import ForgotPasswordPage from "./Components/ForgotPasswordPage";
 
 function App() {
-
-  
-    const [signIn, toggle] = React.useState(true);
-
-    const [showForgotPassword, setShowForgotPassword] = React.useState(false);
+  const [signIn, toggle] = React.useState(true);
+  const [showForgotPassword, setShowForgotPassword] = React.useState(false);
 
   const handleForgotPasswordClick = () => {
     setShowForgotPassword(true);
@@ -17,62 +14,59 @@ function App() {
     setShowForgotPassword(false);
   };
 
-    return (
-        <Components.Container>
-            <Components.SignUpContainer signinIn={signIn}>
-                <Components.Form>
-                    <Components.Title>Create Account</Components.Title>
-                    <Components.Input type='text' placeholder='Name' />
-                    <Components.Input type='email' placeholder='Email' />
-                    <Components.Input type='password' placeholder='Password' />
-                    <Components.Button>Sign Up</Components.Button>
-                </Components.Form>
-            </Components.SignUpContainer>
+  return (
+    <Components.Container>
+      <Components.SignUpContainer signinIn={signIn}>
+        <Components.Form>
+          <Components.Title>Create Account</Components.Title>
+          <Components.Input type='text' placeholder='Name' />
+          <Components.Input type='email' placeholder='Email' />
+          <Components.Input type='password' placeholder='Password' />
+          <Components.Button>Sign Up</Components.Button>
+        </Components.Form>
+      </Components.SignUpContainer>
 
-            <Components.SignInContainer signinIn={signIn}>
-            {!showForgotPassword ? (
-                <Components.Form>
-                    <Components.Title>Sign in</Components.Title>
-                    <Components.Input type='email' placeholder='Email' />
-                    <Components.Input type='password' placeholder='Password' />
-                    <Components.Anchor onClick={handleForgotPasswordClick}>
-                      Forgot your password?
-                    </Components.Anchor>
-                    <Components.Button>Sigin In</Components.Button>
-                </Components.Form>
-                ) : (
-        <ForgotPasswordPage onGoBackClick={handleGoBackClick} />
-      )}
-            </Components.SignInContainer>
+      <Components.SignInContainer signinIn={signIn}>
+        {!showForgotPassword ? (
+          <Components.Form>
+            <Components.Title>Sign in</Components.Title>
+            <Components.Input type='email' placeholder='Email' />
+            <Components.Input type='password' placeholder='Password' />
+            <Components.Anchor onClick={handleForgotPasswordClick}>
+              Forgot your password?
+            </Components.Anchor>
+            <Components.Button>Sigin In</Components.Button>
+          </Components.Form>
+        ) : (
+          <ForgotPasswordPage onGoBackClick={handleGoBackClick} />
+        )}
+      </Components.SignInContainer>
 
-            <Components.OverlayContainer signinIn={signIn}>
-                <Components.Overlay signinIn={signIn}>
+      <Components.OverlayContainer signinIn={signIn}>
+        <Components.Overlay signinIn={signIn}>
+          <Components.LeftOverlayPanel signinIn={signIn}>
+            <Components.Title>Welcome Back!</Components.Title>
+            <Components.Paragraph>
+              To keep connected with us please login with your personal account!
+            </Components.Paragraph>
+            <Components.GhostButton onClick={() => toggle(true)}>
+              Sign In
+            </Components.GhostButton>
+          </Components.LeftOverlayPanel>
 
-                    <Components.LeftOverlayPanel signinIn={signIn}>
-                        <Components.Title>Welcome Back!</Components.Title>
-                        <Components.Paragraph>
-                            To keep connected with us please login with your personal account!
-                        </Components.Paragraph>
-                        <Components.GhostButton onClick={() => toggle(true)}>
-                            Sign In
-                        </Components.GhostButton>
-                    </Components.LeftOverlayPanel>
-
-                    <Components.RightOverlayPanel signinIn={signIn}>
-                        <Components.Title>Hello, Friend!</Components.Title>
-                        <Components.Paragraph>
-                            Enter your personal details to sign up with us!
-                        </Components.Paragraph>
-                        <Components.GhostButton onClick={() => toggle(false)}>
-                            Sigin Up
-                        </Components.GhostButton>
-                    </Components.RightOverlayPanel>
-
-                </Components.Overlay>
-            </Components.OverlayContainer>
-
-        </Components.Container>
-    )
+          <Components.RightOverlayPanel signinIn={signIn}>
+            <Components.Title>Hello, Friend!</Components.Title>
+            <Components.Paragraph>
+              Enter your personal details to sign up with us!
+            </Components.Paragraph>
+            <Components.GhostButton onClick={() => toggle(false)}>
+              Sigin Up
+            </Components.GhostButton>
+          </Components.RightOverlayPanel>
+        </Components.Overlay>
+      </Components.OverlayContainer>
+    </Components.Container>
+  );
 }
 
 export default App;
--- src/Components/ForgotPasswordPage.js
+++ src/Components/ForgotPasswordPage.js
@@ -1,8 +1,13 @@
 import React, { useState } from "react";
-import * as Components from './Auth_Components';
+import { Container, Form, Title, Input, Button, Anchor } from './Auth_Components';
 
-function ForgotPasswordPage() {
+function ForgotPasswordPage({ onGoBackClick }) {
   const [email, setEmail] = useState("");
+
+  const handleGoBack = (event) => {
+    event.preventDefault();
+    onGoBackClick();
+  };
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
@@ -17,21 +22,23 @@ function ForgotPasswordPage() {
   };
 
   return (
-    <Components.Container style={{ position: "relative", overflow: "hidden", height: "100%" }}>
-      <Components.Form onSubmit={handleFormSubmit} style={{ textAlign: 'center' }}> 
-        <Components.Title>Forgot Password</Components.Title>
-        <Components.Input
+    <Container style={{ position: "relative", overflow: "hidden", height: "100%" }}>
+      <Form onSubmit={handleFormSubmit} style={{ textAlign: 'center' }}> 
+        <Title>Forgot Password</Title>
+        <Input
           type="email"
           placeholder="Email"
           value={email}
           onChange={handleEmailChange}
           required
         />
-        <Components.Button type="submit">Reset Password</Components.Button>
-      </Components.Form>
-    </Components.Container>
+        <Button type="submit">Reset Password</Button>
+        <Anchor href="#" onClick={handleGoBack}>
+          Go back to Sign In?
+        </Anchor>
+      </Form>
+    </Container>
   );
-  
 }
 
 export default ForgotPasswordPage;
